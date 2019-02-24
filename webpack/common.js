b22ebac651a7a6471a71853029fdc199e2fcc436
Nikolay <Nazarishin>
2019-02-24 23:14:55 +0200

    fix

--- webpack/common.js
+++ webpack/common.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const path = require('path');
-const env = require('./env');
 
 const isProduction = process.env.NODE_ENV === 'production';
 const isDevelopment = process.env.NODE_ENV === 'development';
