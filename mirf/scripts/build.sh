4c858de55c73296fe80420459b941881ea5c6553
Nickass <nazarishin91@gmail.com>
2020-12-23 02:55:27 +0200

    move demo into main framework directory

--- mirf/scripts/build.sh
+++ mirf/scripts/build.sh
@@ -5,4 +5,5 @@ set -e
 export NODE_ENV=production
 SCRIPT_PATH=$(dirname $(realpath -s $0))
 
-npx webpack --config "$SCRIPT_PATH/webpack/webpack.config.js" $@
+webpack --config "$SCRIPT_PATH/webpack/webpack.config.js" &
+cd demo && npm run build;
--- mirf/scripts/dev.sh
+++ mirf/scripts/dev.sh
@@ -3,11 +3,22 @@
 set -e
 
 SCRIPT_PATH=$(dirname $(realpath -s $0))
+
+export SERVER_HOST="${SERVER_HOST:-localhost}"
+export SERVER_PORT="${SERVER_PORT:-3000}"
+export COMPONENT_SERVER_HOST="${COMPONENT_SERVER_HOST:-localhost}"
+export COMPONENT_SERVER_PORT="${COMPONENT_SERVER_PORT:-8080}"
 export HOT_SERVER_HOST="${HOT_SERVER_HOST:-localhost}"
 export HOT_SERVER_PORT="${HOT_SERVER_PORT:-3001}"
 export PUBLIC_PATH="${PUBLIC_PATH:-"http://$HOT_SERVER_HOST:$HOT_SERVER_PORT/"}"
+export ROOT_COMPONENT="${ROOT_COMPONENT:-"http://$COMPONENT_SERVER_HOST:$COMPONENT_SERVER_PORT/index.js"}"
 export NODE_ENV=development
 
-npx webpack --config "$SCRIPT_PATH/webpack/backend.webpack.config.js" &
-npx webpack serve --config "$SCRIPT_PATH/webpack/frontend.webpack.config.js" &
-nodemon "$SCRIPT_PATH/../dist/server.js" $@
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+  start http:/$SERVER_HOST:$SERVER_PORT
+fi
+
+webpack --config "$SCRIPT_PATH/webpack/backend.webpack.config.js" &
+webpack serve --config "$SCRIPT_PATH/webpack/frontend.webpack.config.js" &
+cd demo && npm run dev -- --host=$COMPONENT_SERVER_HOST --port=$COMPONENT_SERVER_PORT --rc=$ROOT_COMPONENT &
+nodemon "$SCRIPT_PATH/../dist/server.js" $SERVER_HOST $SERVER_PORT $ROOT_COMPONENT;
